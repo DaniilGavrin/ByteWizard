a6df6aba0404193466fca22545f5fc342386f9a0
Daniil <80905799+DaniilGavrin@users.noreply.github.com>
2024-10-07 18:22:04 +0300

    Deploy website - based on 918d15dddd1c61a6467e499466d73352cb9fda28

---
+++ assets/js/acecf23e.b4e3cc16.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbyte_wizard=self.webpackChunkbyte_wizard||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
